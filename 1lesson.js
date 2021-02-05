006edceeef99a446f5ce3418076615f1541946b0
IrinaMelnikova <ira.volvakova@yandex.ru>
2021-02-05 11:04:26 +0300

    Add 1 lesson

---
+++ 1lesson.js
@@ -0,0 +1,16 @@
+// 1 урок 1 задание
+
+//var Tc = 50;
+ 
+//var Tf = (9/5)* Tc + 32;
+//console.log(Tf);
+
+// 3 задание 
+var name = 'Василий';
+var admin = name;
+console.log(admin);
+
+//4 задание
+
+var ans = 1000 + "108";
+console.log(ans);
